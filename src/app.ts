34458a5043ce7a722e38f2e684622a700bd867ac
Alicia <alicia.alexia03@gmail.com>
2026-01-01 21:39:53 -0300

    Fix: configurando CORS para permitir localhost

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import fastifyCors from '@fastify/cors'
 
 export const app = fastify()
 app.register(fastifyCors, {
-  origin: '*',
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true, // ⚠️ ISSO É CRUCIAL: Permite que o cookie vá e volte
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 })
