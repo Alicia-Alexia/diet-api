9f9e23fb3883ff0849a303bc23d38e9cb279943a
Alicia <alicia.alexia03@gmail.com>
2026-01-01 21:47:34 -0300

    Fix: ajustando configuração do banco de dados

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,9 +4,7 @@ import {env} from '../env/index.js'
 export const config: setupKenex.Knex.Config = {
     client: env.DATABASE_CLIENT,
     connection:
-    env.DATABASE_CLIENT === 'sqlite'
-      ? { filename: env.DATABASE_URL }
-      : env.DATABASE_URL,
+    env.DATABASE_CLIENT === 'sqlite'? 'sqlite3' : 'pg',
     useNullAsDefault: true,
     migrations: {
         extension: 'ts',
